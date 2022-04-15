9ab6936771dab3f909611a7692da744ba2c2b55b
Igor Gorlov <gorlov-igorek@yandex.ru>
2022-04-15 23:41:41 +0300

    feat: begin to write registration script

--- architecture.md
+++ architecture.md
@@ -1,1 +1,3 @@
 # Automated Registration System for TeamTalk 5
+
+## Registration Script (reg.php) ~reg
---
+++ reg.php
@@ -0,0 +1,10 @@
+<?php
+
+
+/*
+This script accepts registration data and creates a new TeamTalk 5 account from it.
+© Igor Gorlov, 2022.
+*/
+
+
+?>
