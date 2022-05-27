579c33cfa32a1d39842186912ce16dbd7de50350
Igor Gorlov <gorlov-igorek@yandex.ru>
2022-05-27 12:32:44 +0300

    Add service files for future documentation written in Sphinx

---
+++ docs/conf.py
@@ -0,0 +1,13 @@
+project = 'TeamTalk 5 Registration System'
+copyright = '2022, Igor Gorlov'
+author = 'Igor Gorlov'
+
+templates_path = ['_templates']
+
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+html_theme = 'classic'
+
+html_use_index = False
+
+html_static_path = ['_static']
