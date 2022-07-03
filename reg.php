af7d933edea84c6875d31e1dbe96d0812ed9558f
Igor Gorlov <gorlov-igorek@yandex.ru>
2022-07-03 20:19:39 +0300

    Add beginRegistrationPage() and endRegistrationPage() to reg.php

--- reg.php
+++ reg.php
@@ -49,6 +49,33 @@ function userInfoFromUrl(Configurator $config): UserInfo {
 }
 
 
+/*
+Turns on output buffering and prints the header of the registration page.
+
+From Regsystem's perspective, a "header of a page" is the part of HTML code that precedes main contents of this page;
+not to be confused with <header>, <h1>, or other HTML elements.
+*/
+function beginRegistrationPage(): void {
+	ob_start();
+	echo("<!DOCTYPE html><html lang=\"en\">");
+	echo("<head><meta charset=\"UTF-8\" />");
+	echo("<title>TeamTalk Registration System by Igor Gorlov</title></head>");
+	echo("<body><h1 style=\"text-align: center; text-decoration: underline; font-weight: bold\">");
+	echo("Register a New TeamTalk Account</h1>");
+}
+
+/*
+Prints the footer of the registration page, turns off output buffering, and flushes the buffer.
+
+From Regsystem's perspective, a "footer of a page" is the part of HTML code that succeeds main contents of this page;
+not to be confused with <footer> or other HTML elements.
+*/
+function endRegistrationPage(): void {
+	echo("</body></html>");
+	ob_end_flush();
+}
+
+
 // Configure the essential options.
 $config = new Configurator("config.json");
 $serverName = "";
