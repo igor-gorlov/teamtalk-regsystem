200c2203a91ed21d2a55a4369172a711ceb3a032
Igor Gorlov <gorlov-igorek@yandex.ru>
2022-07-14 12:10:11 +0300

    Initially add json.php

---
+++ json.php
@@ -0,0 +1,12 @@
+<?php
+
+
+/*
+An object-oriented API for reading, writing, and manipulating JSON.
+Built on top of the standard PHP functions related to JSON and filesystem.
+
+© Igor Gorlov, 2022.
+*/
+
+
+declare(strict_types = 1);
