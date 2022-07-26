bec0dbecaff68982265643dd158f95381e778cec
Igor Gorlov <gorlov-igorek@yandex.ru>
2022-07-26 09:10:58 +0300

    Add enumeration with TeamTalk 5 user types

--- server.php
+++ server.php
@@ -41,6 +41,13 @@ class ServerInfo {
 	}
 }
 
+// TeamTalk 5 user type.
+enum UserType: int {
+	case NONE = 0;
+	case DEFAULT = 1;
+	case ADMIN = 2;
+}
+
 // Encapsulates TeamTalk 5 user information.
 class UserInfo {
 
