4f7a000cb600055aad93745c1ffb1af82d90ffc4
Igor Gorlov <gorlov-igorek@yandex.ru>
2022-08-21 12:49:46 +0300

    Move printErrorMessage() from error.php to ui.php

--- ui.php
+++ ui.php
@@ -57,3 +57,13 @@ function showRegistrationForm(array $servers): void {
 	echo("<div><button type=\"submit\" name=\"form\" value=\"1\">Register now!</button></div>");
 	ob_end_flush();
 }
+
+// The default handler for uncaught exceptions and errors. Displays a pretty formatted message in a browser.
+function printErrorMessage(Throwable $e): void {
+	ob_start();
+	echo("<table><tr><td style=\"color: red\"><strong>Error!</strong></td><td><pre><code>");
+	echo($e->getMessage());
+	echo("<br>(" . $e->getFile() . " : " . $e->getLine() . ")");
+	echo("</code></pre></td></tr></table>");
+	ob_end_flush();
+}
