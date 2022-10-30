81b63601e76666ad8dc1c7deb6ef63af0151683e
Igor Gorlov <gorlov-igorek@yandex.ru>
2022-10-30 17:55:55 +0300

    Refactor reg.php
    The script has been explicitly divided into logical parts according to
    various types of URL requests it handles: a section for incoming account
    properties and another section for an empty request (the latter displays
    a registration form).

--- reg.php
+++ reg.php
@@ -25,53 +25,55 @@ require_once "ui.php";
 use Twig\Environment as TwigEnvironment;
 use Twig\Loader\FilesystemLoader as TwigFilesystemLoader;
 
-// Set up GUI.
 set_exception_handler("printErrorMessage");
 ini_set("intl.use_exceptions", true);
+
+// Prepare common services needed for all operations.
 $view = new TwigEnvironment(new TwigFilesystemLoader("templates/"));
 $validator = new Validator;
 $locale = Locale::acceptFromHttp($_SERVER["HTTP_ACCEPT_LANGUAGE"]);
 $langpack = new LanguagePack($validator, $locale);
-
-// Configure the essential options.
 $config = new Configurator($validator, new Json("config.json"));
 $allServers = $config->getAllServersInfo();
-$newAccount = null;
-try {
-	$newAccount = UserInfo::fromUrl($validator, $allServers);
-}
-catch(BadQueryStringException $e) {
-	if(!isset($_GET["form"])) {
-		echo $view->render("reg_form.html", array(
+
+// Try to create a new account directly from its properties passed via URL.
+if(isset($_GET["form"])) {
+	$newAccount = null;
+	try {
+		$newAccount = UserInfo::fromUrl($validator, $allServers);
+	}
+	catch(BadQueryStringException $e) {
+		echo $view->render("reg_results.html", array(
 			"langpack" => $langpack,
-			"servers" => $allServers
+			"succeeded" => false,
+			"invalidUrlParams" => $e->invalidUrlParams
+		));
+		exit();
+	}
+	$systemAccount = $config->getSystemAccountInfo($newAccount->server->name);
+	$registrator = new AccountManager($validator, new Tt5Session($systemAccount));
+	try {
+		$registrator->createAccount($newAccount);
+	}
+	catch(AccountAlreadyExistsException) {
+		echo $view->render("reg_results.html", array(
+			"langpack" => $langpack,
+			"succeeded" => false,
+			"newAccount" => $newAccount
 		));
 		exit();
 	}
 	echo $view->render("reg_results.html", array(
 		"langpack" => $langpack,
-		"succeeded" => false,
-		"invalidUrlParams" => $e->invalidUrlParams
-	));
-	exit();
-}
-$systemAccount = $config->getSystemAccountInfo($newAccount->server->name);
-
-// Create a new account.
-$registrator = new AccountManager($validator, new Tt5Session($systemAccount));
-try {
-	$registrator->createAccount($newAccount);
-}
-catch(AccountAlreadyExistsException) {
-	echo $view->render("reg_results.html", array(
-		"langpack" => $langpack,
-		"succeeded" => false,
+		"succeeded" => true,
 		"newAccount" => $newAccount
 	));
-	exit();
 }
-echo $view->render("reg_results.html", array(
-	"langpack" => $langpack,
-	"succeeded" => true,
-	"newAccount" => $newAccount
-));
+
+// Display a registration form.
+else {
+	echo $view->render("reg_form.html", array(
+		"langpack" => $langpack,
+		"servers" => $allServers
+	));
+}
