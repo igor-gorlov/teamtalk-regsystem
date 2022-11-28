b9cb339ba4002d59fd67ad5d1676c8629bff8390
Igor Gorlov <gorlov-igorek@yandex.ru>
2022-11-28 14:58:44 +0300

    Construct InvalidConfigException from a filename
    Previously, an instance of this class was based on an arbitrary message.

--- error.php
+++ error.php
@@ -80,8 +80,8 @@ class InvalidJsonException extends RuntimeException {
 
 // Is thrown on a problem with configuration.
 class InvalidConfigException extends RuntimeException {
-	public function __construct(string $message) {
-		parent::__construct($message);
+	public function __construct(string $filename) {
+		parent::__construct("Invalid configuration file \"$filename\"");
 	}
 }
 
